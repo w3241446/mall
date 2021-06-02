bad823e3ae6231c9c6e48365ce03c09f5fddd866
liuyinghao <liuyinghao@csii.com.cn>
2021-06-02 14:31:45 +0800

    基本搭建

---
+++ src/main/java/com/mall/lyh/service/PmsProductService.java
@@ -0,0 +1,8 @@
+package com.mall.lyh.service;
+
+/**
+ * 商品管理业务层
+ * @author lyh 2021-05-31
+ */
+public interface PmsProductService {
+}
---
+++ src/main/java/com/mall/lyh/service/impl/PmsProductServiceImpl.java
@@ -0,0 +1,8 @@
+package com.mall.lyh.service.impl;
+
+/**
+ * 商品管理业务层
+ * @author lyh 2021-05-31
+ */
+public class PmsProductServiceImpl {
+}
